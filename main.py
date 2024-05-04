23c608ce55b8e3652d88b3d8801d6bce9f7e8912
Kevin <realkevinx@gmail.com>
2024-05-04 14:23:48 -0400

    streak

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
 import os
+import json
+
+
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix=",", intents=discord.Intents.all())
+
+def get_streak():
+    with open("streak.json", "r") as f:
+        return json.load(f)["streak"]
+
+def write_streak(n):
+    js = {"streak": n}
+    out = json.dunps(js, indent = 4)
+    
+    with open("streak.json", "w") as f:
+        f.write(out)
+
+streak = 0
+
 
 @bot.event
 async def on_ready():
@@ -39,5 +56,17 @@ async def kanbanmove(interaction: discord.Interaction, task: str):
 @bot.tree.command(name="kanbanclear")
 async def kanbanclear(interaction: discord.Interaction):
     await interaction.response.send_message("cleared kanban board")
+
+@bot.tree.command(name="kanbangetstreak")
+async def kanbanstreak(interaction: discord.Interaction):
+    streak = get_streak()
+    await interaction.response.send_message(f"Streak:{streak}")
+
+@bot.tree.command(name="kanbanaddstreak")
+async def kanbanaddstreak(interaction: discord.Interaction):
+    streak = get_streak()
+    streak += 1
+    write_streak(streak)
+    await interaction.response.send_message(f"Streak:{streak}")
 
 bot.run(TOKEN)
